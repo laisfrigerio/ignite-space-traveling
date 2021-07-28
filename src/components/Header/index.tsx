7615cf82919755a35b643cbe84e2bca0b6464c84
Lais Frigério <laiss@gazetadopovo.com.br>
2021-07-27 22:30:49 -0300

    Global style and Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
-  // TODO
+import Link from 'next/link'
+import styles from './header.module.scss'
+import commonStyles from '../../styles/common.module.scss'
+
+export default function Header () {
+  return (
+    <header className={styles.headerContainer}>
+      <nav className={commonStyles.headerNav}>
+        <Link href="/">
+          <a>
+            <img src="/logo.svg" alt="Logo space traveling" />
+          </a>
+        </Link>
+      </nav>
+    </header>
+  )
 }
